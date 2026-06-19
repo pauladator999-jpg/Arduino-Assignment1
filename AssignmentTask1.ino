aac205461fef64c41768b7ccf929542df648fb63
pauladator999-jpg <pauladator999@gmail.com>
2026-06-19 13:33:38 +0000

    AssignmentTask1.ino
    Add final countdown sketch

--- AssignmentTask1.ino
+++ AssignmentTask1.ino
@@ -1,36 +1,31 @@
 int ledPin = 13;
-int onTime = 500;
-int offTime = 500;
 
-void setup(){ pinMode(ledPin, OUTPUT);
+void setup(){
+  pinMode(ledPin, OUTPUT);
   Serial.begin(9600);
-  Serial.println("Blink started");
- 
-  int myAge = 21;
-  float myHeight = 1.75;
-  String myName = "Type your name here";
-  bool isStudent = true;
 
-  Serial.print("My name is: ");
-  Serial.println(myName);
-
-  Serial.print("My age is: ");
-  Serial.println(myAge);
-
-  Serial.print("My height is: ");
-  Serial.print(myHeight);
-  Serial.println(" meters");
-
-  Serial.print("Am I a student?");
-  Serial.print(isStudent);
-}
-void loop(){
- digitalWrite(ledPin, HIGH);
-  Serial.println("LED is ON");
-  delay(onTime);
-
-  digitalWrite(ledPin, LOW);
-  Serial.println("LED is OFF");
-  delay(offTime);
+  Serial.println("=== Countdown beginning ===");
   
-}
+  int counter = 5;   //start the counter at 5
+  
+  while(counter > 0) {
+    Serial.print("Counter is: ");
+    Serial.println(counter);
+
+    delay(1000);
+    counter = counter - 1;
+    }  
+  
+  Serial.println("Liftoff!");
+  
+  // Flash the LED 3 times to celebrate
+  digitalWrite(ledPin, HIGH); delay(200); digitalWrite(ledPin, LOW); delay(200);
+  digitalWrite(ledPin, HIGH); delay(200); digitalWrite(ledPin, LOW); delay(200);
+  digitalWrite(ledPin, HIGH); delay(200); digitalWrite(ledPin, LOW); delay(200);
+  }
+
+
+
+void loop(){
+  //Empty - countdown only runs once in setup()
+  }
